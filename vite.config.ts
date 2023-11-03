af1a028d7c5fbeec97d6eccffaedb479d305b681
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-11-03 21:41:57 +0300

    build(sw): update vite-plugin-pwa config optionss

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({ registerType: "autoUpdate", injectRegister: "script" }),
+  ],
 });
