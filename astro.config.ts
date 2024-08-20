2caf25dfd7265fa8f45dabc3365836c435ea0406
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-20 12:54:11 +0300

    build: add svelte integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,7 @@
+import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte()]
+});
