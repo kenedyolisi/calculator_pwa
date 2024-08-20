d50289846b268561b9960b0f38d1b900cd040bdd
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-20 15:11:52 +0300

    build: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,16 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+
 export default [
+  // add more generic rule sets here, such as:
+  js.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
+  ...eslintPluginSvelte.configs["flat/recommended"],
   {
     rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error",
+      // "svelte/rule-name": "error",
       semi: "error",
       "prefer-const": "error",
     },
