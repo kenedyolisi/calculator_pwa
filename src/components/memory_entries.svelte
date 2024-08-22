df1dde2107654fd11a6c72d0cb8c16954a3665a8
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-22 23:56:39 +0300

    feat(components): create memory entries

---
+++ src/components/memory_entries.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import MemoryEntry from "@components/memory_entry.svelte";
+  import { memoryEntries } from "src/store";
+</script>
+
+{#if $memoryEntries.length}
+  <ul>
+    {#each $memoryEntries as memoryEntry, index (index)}
+      <MemoryEntry {memoryEntry} />
+    {/each}
+  </ul>
+{:else}
+  <p class="text-center text-sm">There is nothing in memory yet.</p>
+{/if}
