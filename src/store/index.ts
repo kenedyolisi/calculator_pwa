8c230f62f161561993104b0d14186a3415e1604a
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-23 00:03:41 +0300

    feat(utils): update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,13 @@
 import { writable, type Writable } from "svelte/store";
 
-export const chronicles: Writable<Chronicle[]> = writable([]);
+export interface IHistoryEntry {
+  expression: string;
+  result: string;
+}
+
+export const historyEntries: Writable<IHistoryEntry[]> = writable([
+  { expression: "12 + 5 = ", result: "17" },
+]);
+
+export const memoryEntries: Writable<string[]> = writable([]);
 
