dfae92133ec62fd16e86bf9ad25c677b94151524
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-22 22:20:13 +0300

    refactor(components): update history entry

--- src/components/history_entry.svelte
+++ src/components/history_entry.svelte
@@ -1,21 +1,18 @@
-<script context="module">
-  export type Datum = { expression: string; result: string };
-</script>
-
 <script lang="ts">
+  import type { IHistoryEntry } from "src/store";
   import { slide } from "svelte/transition";
 
   interface Props {
-    chronicle: Chronicle;
+    historyEntry: IHistoryEntry;
   }
 
-  let { chronicle }: Props = $props();
+  let { historyEntry }: Props = $props();
 </script>
 
 <div
   class="flex flex-col items-end rounded-lg px-3 py-1 text-end hover:bg-slate-100"
   in:slide
 >
-  <div>{chronicle.expression}</div>
-  <div>{chronicle.result}</div>
+  <div aria-label="expression">{historyEntry.expression}</div>
+  <div aria-label="result">{historyEntry.result}</div>
 </div>
