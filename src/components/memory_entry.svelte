a6a9e30b8cfb5fd4c0dde1ec96a04d1dc576cc7d
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-22 23:56:13 +0300

    feat(components): create memory entry

---
+++ src/components/memory_entry.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  interface Props {
+    memoryEntry: string;
+  }
+  let { memoryEntry }: Props = $props();
+</script>
+
+<li class="rounded-lg px-3 py-2 hover:bg-slate-200">
+  {memoryEntry}
+</li>
