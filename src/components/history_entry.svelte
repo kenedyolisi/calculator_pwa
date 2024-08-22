aba77f8b5a7792b7254e0dd219c3c3fd2d52a896
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-08-23 00:08:12 +0300

    refactor(components): update history entry

--- src/components/history_entry.svelte
+++ src/components/history_entry.svelte
@@ -9,10 +9,10 @@
   let { historyEntry }: Props = $props();
 </script>
 
-<div
+<li
   class="flex flex-col items-end rounded-lg px-3 py-1 text-end hover:bg-slate-100"
   in:slide
 >
   <div aria-label="expression">{historyEntry.expression}</div>
   <div aria-label="result">{historyEntry.result}</div>
-</div>
+</li>
